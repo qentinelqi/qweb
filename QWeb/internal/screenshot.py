9ef9d0ee167362165c5edc98400a369c1fbff5a9
Tuomas Koukkari <tuomas.koukkari@qentinel.com>
2022-04-06 14:10:27 +0300

    innerHeight changed to window.innerHeight

--- QWeb/internal/screenshot.py
+++ QWeb/internal/screenshot.py
@@ -307,7 +307,7 @@ def chromium_full_screenshot(driver, filepath):
                        "width: Math.max(window.innerWidth, "
                        "document.body.scrollWidth, "
                        "document.documentElement.scrollWidth)|0,"
-                       "height: Math.max(innerHeight, document.body.scrollHeight, "
+                       "height: Math.max(window.innerHeight, document.body.scrollHeight, "
                        "document.documentElement.scrollHeight)|0,"
                        "deviceScaleFactor: window.devicePixelRatio || 1,"
                        "mobile: typeof window.orientation !== 'undefined'"
