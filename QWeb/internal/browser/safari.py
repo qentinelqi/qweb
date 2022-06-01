26060f3390b658de4350d23c7745650742b1b139
Mikko Turunen <turunen.m@gmail.com>
2022-06-01 12:16:09 +0300

    Add short implicit_wait to Safaridriver

--- QWeb/internal/browser/safari.py
+++ QWeb/internal/browser/safari.py
@@ -19,6 +19,7 @@ def open_browser(port: int = 0,
     desired_capabilities = DesiredCapabilities.SAFARI
 
     driver = webdriver.Safari(port, executable_path, reuse_service, desired_capabilities, quiet)
+    driver.implicitly_wait(0.1)
     driver.maximize_window()
     browser.cache_browser(driver)
     open_windows.append(driver.current_window_handle)
