3f877a3205b953871811d98f29288b289dfde264
Mikko Turunen <turunen.m@gmail.com>
2024-04-22 18:21:07 +0300

    add .pabot_suite_order update .gitignore

--- duties.py
+++ duties.py
@@ -92,7 +92,7 @@ def acceptance_tests(ctx,
 
     cmd_str = remove_extra_whitespaces(
                f" {python_exe} -m pabot.pabot"
-               f" --ordering test/.pabotsuitenames"
+               f" --ordering test/acceptance/.pabot_suite_order"
                f" {listener_cmd}"
                f" {cmd_exit_on_failure}"
                f" -v BROWSER:{browser}"
