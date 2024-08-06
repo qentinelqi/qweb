5d9be795bfe4c7b3150b31db3eba98c379171cfb
Tuomas Koukkari <tkoukkari@copado.com>
2024-08-06 11:45:13 +0300

    chore: added Python 3.12 to setup.py versions

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: Apache Software License',
         'Framework :: Robot Framework',
         'Framework :: Robot Framework :: Library'
