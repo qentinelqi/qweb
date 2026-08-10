3f0d2e3eeba7bd6210629e342108437aa58363de
Tuomas Koukkari <tkoukkari@copado.com>
2026-08-10 15:14:37 +0300

    another trial

--- QWeb/internal/browser/edge.py
+++ QWeb/internal/browser/edge.py
@@ -79,8 +79,8 @@ def create_edge_options(edge_args: Optional[list[str]], **kwargs: Any) -> Option
 
     # app_armor_restricted = user.apparmor_userns_restricted()
     # if app_armor_restricted:
-    #     logger.info("Linux Edge detected with AppArmor user namespace restrictions enabled.", also_console=True)
-    #     logger.info("Adding --no-sandbox to work around Chromium sandbox initialization.", also_console=True)
+    #     logger.info("Linux Edge detected with AppArmor user namespace restrictions enabled.")
+    #     logger.info("Adding --no-sandbox to work around Chromium sandbox initialization.")
     if user.is_root() or user.is_docker():  # or app_armor_restricted:
         options.add_argument("--no-sandbox")  # pylint: disable=no-member
     if edge_args:
